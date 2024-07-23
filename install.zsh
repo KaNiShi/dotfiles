df8f3f2d69679123f433662c4c9a78ee04287162
KaNiShi <kanishi.kuu@gmail.com>
2024-07-23 15:59:16 +0000

    てーまついか

--- install.zsh
+++ install.zsh
@@ -8,6 +8,7 @@ _SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 echo 'Backup exists files.'
 [[ -f "$HOME/.zshrc" && ! -L "$HOME/.zshrc" ]] && mv -v "$HOME/.zshrc" "$_SCRIPT_DIR/.zsh/backups/"
 [[ -f "$HOME/.zprofile" && ! -L "$HOME/.zprofile" ]] && mv -v "$HOME/.zprofile" "$_SCRIPT_DIR/.zsh/backups/"
+[[ -f "$HOME/.p10k.zsh" && ! -L "$HOME/.p10k.zsh" ]] && mv -v "$HOME/.p10k.zsh" "$_SCRIPT_DIR/.zsh/backups/"
 
 if [ ! -d "$XDG_DATA_HOME" ]; then
     echo "Create $XDG_DATA_HOME"
@@ -17,4 +18,5 @@ fi
 echo 'Install dotfiles.'
 ln -vnsf "$_SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
 ln -vnsf "$_SCRIPT_DIR/.zprofile" "$HOME/.zprofile"
+ln -vnsf "$_SCRIPT_DIR/.p10k.zsh" "$HOME/.p10k.zsh"
 ln -vnsf "$_SCRIPT_DIR/.zsh" "$XDG_DATA_HOME/dotfiles.zsh"
