4f47758c88cd8117c27eb712e02881d22b9c75c4
KaNiShi <kanishi.kuu@gmail.com>
2024-09-13 16:48:41 +0000

    めっせーじちょうせい

--- install.zsh
+++ install.zsh
@@ -5,7 +5,7 @@ set -eu
 XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 _SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
-echo 'Backup exists files.'
+echo '### Backup exists files. ###'
 NOW=$(date +%Y%m%d%H%M%S)
 [[ -f "$HOME/.zshrc" && ! -L "$HOME/.zshrc" ]] && mv -v "$HOME/.zshrc" "$HOME/.zshrc"
 [[ -f "$HOME/.zprofile" && ! -L "$HOME/.zprofile" ]] && mv -v "$HOME/.zprofile" "$HOME/.zprofile.$NOW"
@@ -15,10 +15,10 @@ if [ ! -d "$XDG_DATA_HOME" ]; then
     mkdir -p "$XDG_DATA_HOME"
 fi
 
-echo 'Install dotfiles.'
+echo '### Install dotfiles. ###'
 ln -vnsf "$_SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
 ln -vnsf "$_SCRIPT_DIR/.zprofile" "$HOME/.zprofile"
 ln -vnsf "$_SCRIPT_DIR/zsh" "$XDG_DATA_HOME/dotfiles.zsh"
 
-echo 'Remove oldfiles.'
+echo '### Remove oldfiles. ###'
 [ -L "$HOME/.p10k.zsh" ] && unlink "$HOME/.p10k.zsh" && echo "Unlinked: $HOME/.p10k.zsh"
