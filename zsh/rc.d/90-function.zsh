dcc697040134ee0040b4e383d5fb7633579b3735
KaNiShi <kanishi.kuu@gmail.com>
2024-11-05 02:05:01 +0000

    にんしょうしょりのついか

--- zsh/rc.d/90-function.zsh
+++ zsh/rc.d/90-function.zsh
@@ -23,4 +23,16 @@ if (( $+commands[aws] )); then
 
         export AWS_PROFILE=$1
     }
+
+    aws_sts-assume-role-with-saml() {
+        aws_sts_credentials_set "$1" "$(aws sts assume-role-with-saml --role-arn "$2" --principal-arn "$3" --saml-assertion "$4")"
+    }
+
+    aws_sts_credentials_set() {
+        read access secret session <<< $(echo "$2" | jq -r '.Credentials | [ .AccessKeyId, .SecretAccessKey, .SessionToken] | join(" ")')
+        aws configure set --profile "$1" aws_access_key_id "$access"
+        aws configure set --profile "$1" aws_secret_access_key "$secret"
+        aws configure set --profile "$1" aws_session_token "$session"
+        echo "Credentials set to $1."
+    }
 fi
