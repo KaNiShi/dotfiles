4014a28516b73e947dd291a377fa9a41c97e4e29
KaNiShi <kanishi.kuu@gmail.com>
2025-11-26 02:10:44 +0900

    いんすとーるすくりぷとのこうしん

--- install.zsh
+++ install.zsh
@@ -10,8 +10,7 @@ _SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
 echo '### Backup exists files. ###'
 NOW=$(date +%Y%m%d%H%M%S)
-[[ -f "$HOME/.zshrc" && ! -L "$HOME/.zshrc" ]] && mv -v "$HOME/.zshrc" "$HOME/.zshrc"
-[[ -f "$HOME/.zprofile" && ! -L "$HOME/.zprofile" ]] && mv -v "$HOME/.zprofile" "$HOME/.zprofile.$NOW"
+[[ -f "$HOME/.zshenv" && ! -L "$HOME/.zshenv" ]] && mv -v "$HOME/.zshenv" "$HOME/.zshenv.$NOW"
 
 echo '### Create directories. ###'
 if [ ! -d "$XDG_DATA_HOME" ]; then
@@ -23,8 +22,7 @@ if [ ! -d "$ZSH_CACHE_DIR" ]; then
 fi
 
 echo '### Install dotfiles. ###'
-ln -vnsf "$_SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
-ln -vnsf "$_SCRIPT_DIR/.zprofile" "$HOME/.zprofile"
+ln -vnsf "$_SCRIPT_DIR/.zshenv" "$HOME/.zshenv"
 ln -vnsf "$_SCRIPT_DIR/zsh" "$XDG_DATA_HOME/dotfiles.zsh"
 
 if (( $+commands[anyenv] )); then
@@ -32,8 +30,5 @@ if (( $+commands[anyenv] )); then
     [ ! -d "$ANYENV_ROOT/plugins" ] && mkdir -pv "$ANYENV_ROOT/plugins"
     ln -vnsf "$_SCRIPT_DIR/utils/anyenv/anyenv-init-cache" "$ANYENV_ROOT/plugins/anyenv-init-cache"
 fi
-
-echo '### Remove oldfiles. ###'
-[ -L "$HOME/.p10k.zsh" ] && unlink "$HOME/.p10k.zsh" && echo "Unlinked: $HOME/.p10k.zsh"
 
 echo '\e[32mInstall success.\e[m'
