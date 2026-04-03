37ad3d68f1c3a204d9f5a66809bd08a40929d872
KaNiShi <35655549+KaNiShi@users.noreply.github.com>
2026-04-04 01:01:41 +0900

    えにーえんぶさくじょ

--- zsh/rc.lazy.d/20-anyenv.zsh
+++
@@ -1,8 +0,0 @@
-if (( $+commands[anyenv] )); then
-    eval "$(anyenv init-cache-load)"
-
-    if (( $+commands[nodenv] )); then
-        NODENV_ROOT="$(nodenv root)"
-        [ -d "$NODENV_ROOT/completions" ] && FPATH="$NODENV_ROOT/completions:$FPATH"
-    fi
-fi
